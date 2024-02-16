59c872fbcbfb65517de83f06190d92d5440a7668
Joshua Barron <jrbarron@eos.beocat.ksu.edu>
2024-02-15 21:46:29 -0600

    Added skeleton to analysis

--- src/analysis.c
+++ src/analysis.c
@@ -19,7 +19,51 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    abort();  // replace me with implementation.
+    //abort();  // replace me with implementation.
+    /* Create Ready Queue */
+    dyn_array_t* ready_queue;
+    ready_queue = load_process_control_blocks(argv(0));
+
+    ScheduleResult_t* time_data;
+
+    /* Run the First Come First Serve algorithm */
+    if(strcmp(argv[1], FCFS) == 0){
+        if(!first_come_first_serve(ready_queue, time_data)){
+            printf("Scheduling Algorithm Failed");
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* Milestone says to do shortest time remaining first, not priority */
+    else if(strcmp(argv[1], P) == 0){
+        if(!priority(ready_queue, time_data)){
+            printf("Scheduling Algorithm Failed");
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* Run the Round Robin Algorithm */
+    else if(strcmp(argv[1], RR) == 0){
+        if(!round_robin(ready_queue, time_data)){
+            printf("Scheduling Algorithm Failed");
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* Run the Shortest Job First Algorithm */
+    else if(strcmp(argv[1], SJF) == 0){
+        if(!shortest_job_first(ready_queue, time_data)){
+            printf("Scheduling Algorithm Failed");
+            return EXIT_FAILURE;
+        }
+    }
+
+    /* The command line did not give a valid schedule algorithm input */
+    else{
+        printf("Invalid schedule algorithm");
+        return EXIT_FAILURE;
+    }
+
 
     return EXIT_SUCCESS;
 }
